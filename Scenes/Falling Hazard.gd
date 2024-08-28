8fe68611fafeb0063b27b1e5f2b034a59d5de902
CombustibleToast <34487106+CombustibleToast@users.noreply.github.com>
2024-08-28 14:34:03 -0400

    basic hazard spawning

--- Scenes/Falling Hazard.gd
+++ Scenes/Falling Hazard.gd
@@ -4,7 +4,7 @@ var fall_direction: Vector3 = Vector3(0,-1,0) #Set by spawner when spawned, defa
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	linear_velocity = fall_direction
-	angular_velocity = Vector3(randf_range(0,1), randf_range(0,1), randf_range(0,1)).normalized()
+	angular_velocity = Vector3(randf_range(-1,1), randf_range(-1,1), randf_range(-1,1)).normalized()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
---
+++ Scenes/hazard_spawner.gd
@@ -0,0 +1,44 @@
+extends Node3D
+
+# Spawning Object
+@export var hazard_object: PackedScene
+const SPAWN_Z_RANGE: Array = [-7,7]
+const SPAWN_HEIGHT: float = 5
+
+# Spawn Time
+@onready var spawning: bool = true # used to pause the countdown timer for spawning
+const MIN_SPAWN_TIME: float = 1
+const MAX_SPAWN_TIME: float = 3
+@onready var next_spawn_time = 0
+@onready var time_elapsed = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	handle_spawning(delta)
+
+func handle_spawning(delta):
+	# Do nothing if no spawning
+	if !spawning:
+		return
+
+	time_elapsed += delta
+
+	# If the time elapsed hasn't been passed, finish here
+	if time_elapsed < next_spawn_time:
+		return
+
+	# Spawn the hazard
+	var new_hazard: Node3D = hazard_object.instantiate()
+	new_hazard.position = Vector3(0, SPAWN_HEIGHT, randf_range(SPAWN_Z_RANGE[0], SPAWN_Z_RANGE[1]))
+	add_child(new_hazard)
+
+	# Reset timer
+	time_elapsed = 0
+	next_spawn_time = randf_range(MIN_SPAWN_TIME, MAX_SPAWN_TIME)
+
+	print("Spawned hazard @ %s, next in %s seconds" % [new_hazard.position, next_spawn_time])
